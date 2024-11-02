c2dca7dea6c3a2b3ebb0201da4b5829213d8dfa3
tsh235 <tanyashustrova@yandex.ru>
2024-11-02 18:30:32 +0300

    edit index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const server = startServer();
 
-server.listen(PORT, 'localhost', () => {
+server.listen(PORT, () => {
 	console.log(`Сервер запущен на порте ${PORT}`);
 });
